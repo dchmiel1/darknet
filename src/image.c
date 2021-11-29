cfbdfbebd077dbbef3cb24fffb5a92be0faf24ee
dchmiel1 <daniel.chmielewiec@gmail.com>
2021-11-29 17:51:12 +0100

    fix

--- src/image.c
+++ src/image.c
@@ -341,7 +341,7 @@ void draw_detections_v3(image im, detection *dets, int num, float thresh, char *
         const int best_class = selected_detections[i].best_class;
         //printf("%s: %.0f%%", names[best_class],    selected_detections[i].det.prob[best_class] * 100);
 
-        printf("%s %d %d %d %d", 
+        printf("%s %.0f %.0f %.0f %.0f", 
             names[best_class],
             round((selected_detections[i].det.bbox.x - selected_detections[i].det.bbox.w / 2)*im.w),
             round((selected_detections[i].det.bbox.y - selected_detections[i].det.bbox.h / 2)*im.h),
